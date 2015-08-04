4f7c26845684bdc879be2ea1c04b7551bda500fc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2015-08-04 21:13:49 +0800

    Listener model

--- PoiDBReporter/Reporter.cs
+++ PoiDBReporter/Reporter.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models.Raw;
 
 namespace Huoyaoyuan.KCVPlugins.PoiDBReporter
 {
     class Reporter
     {
         public bool EnableReporter { get; set; }
+        public Reporter()
+        {
+            var proxy = KanColleClient.Current.Proxy;
+            proxy.api_req_kousyou_createitem.TryParse<kcsapi_createitem>().Subscribe(x => this.CreateItem(x.Data, x.Request));
+        }
+        void CreateItem(kcsapi_createitem api,NameValueCollection request)
+        {
+
+        }
     }
 }
